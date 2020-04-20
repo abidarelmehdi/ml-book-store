3906d9153a99232129b14698075343a767a73cdb
Abidar El Mehdi <abidar.mehdi95@gmail.com>
2020-04-20 13:24:36 +0000

    add profile picture to user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,13 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+from core.custom.model_fields import LowerCharField
 
 
 class CustomUser(AbstractUser):
-    pass
+    profile_picture = models.ImageField(
+        "Profile picture", upload_to="profile/%Y/%m/%d"
+    )
+    birthday = models.DateField("Birthday", blank=True, null=True)
+    twitter = LowerCharField("Twitter Account", blank=True, null=True)
+    linkedin = LowerCharField("LinkedIn Account", blank=True, null=True)
