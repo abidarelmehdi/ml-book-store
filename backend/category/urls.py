5769eb4d84f8152242a959c410f88fbdc8760597
Abidar El Mehdi <abidar.mehdi95@gmail.com>
2020-04-21 01:15:16 +0000

    test category apis

--- backend/category/urls.py
+++ backend/category/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from category.api import CategoryViewSet
 
-
+app_name = "categories"
 router = DefaultRouter()
 router.register("", CategoryViewSet)
 
