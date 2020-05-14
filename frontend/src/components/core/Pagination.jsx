f4cc4104c7a5472d06962d58ca23904755197017
Abidar El Mehdi <abidar.mehdi95@gmail.com>
2020-05-14 02:50:40 +0000

    fix pagination appending params issue

--- frontend/src/components/core/Pagination.jsx
+++ frontend/src/components/core/Pagination.jsx
@@ -3,17 +3,21 @@ import { Link } from "react-router-dom";
 import { getParamFromUrl } from "../../common/functions";
 import { withRouter } from "react-router-dom";
 
-function Pagination({ data, offset = 2, history }) {
+function Pagination({ data, history }) {
   const [pageNum, setPageNum] = useState(1);
+  let currentUrl = history.location.search;
+  currentUrl = currentUrl.includes("?") ? currentUrl + "&" : currentUrl + "?";
+
   useEffect(() => {
     setPageNum(getParamFromUrl("page"));
   }, [pageNum]);
+
   return (
     <nav className="mt-10 border-t border-gray-200 px-4 flex items-center justify-between sm:px-0">
       <div className="w-0 flex-1 flex">
         {data.previous && (
           <Link
-            to={`?page=${data.page - 1}`}
+            to={`${currentUrl}page=${data.page - 1}`}
             className="-mt-px border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm leading-5 font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-400 transition ease-in-out duration-150"
           >
             Previous
@@ -22,7 +26,7 @@ function Pagination({ data, offset = 2, history }) {
       </div>
       <div className="hidden md:flex">
         <Link
-          to={`?page=${data.page}`}
+          to={`${currentUrl}page=${data.page}`}
           className="-mt-px border-t-2 border-transparent pt-4 px-4 inline-flex items-center text-sm leading-5 font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-400 transition ease-in-out duration-150"
         >
           {data.page}
@@ -31,7 +35,7 @@ function Pagination({ data, offset = 2, history }) {
       <div className="w-0 flex-1 flex justify-end">
         {data.next && (
           <Link
-            to={`?page=${data.page + 1}`}
+            to={`${currentUrl}page=${data.page + 1}`}
             className="-mt-px border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm leading-5 font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-400 transition ease-in-out duration-150"
           >
             Next
